3eebfa1c31dfcadb400b06712330738b9f2a5efc
ClareMcDonald <clare.s.mcdonald@gmail.com>
2021-10-07 11:15:14 -0700

    test work

--- test/tests.js
+++ test/tests.js
@@ -3,17 +3,41 @@
 
 const test = QUnit.test;
 
+function add(num1, num2) {
+    return num1 + num2;
+}
+ 
 // name your test by what it is testing
-test('time to test a function', (expect) => {
-    //Arrange
+test('add two numbers', (expect) => {
+    // 1.Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const x = 4;
+    const y = 4;
+    const expected = 8;
     
-    //Act 
+    
+    // 2.Act 
     // Call the function you're testing and set the result to a const
-    const actual = true; // use your function here
+    const actual = add(x, y); // use your function here
 
-    //Expect
+    // 3.Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('add two numbers', (expect) => {
+    // 1.Arrange
+    // Set up your arguments and expectations
+    const x = 4;
+    const y = 3;
+    const expected = 7;
+    
+    
+    // 2.Act 
+    // Call the function you're testing and set the result to a const
+    const actual = add(x, y); // use your function here
+
+    // 3.Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
